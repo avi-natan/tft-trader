8c0ff1b49af87a01fbd6daa3820b864eb40315b0
avi-natan <47332681+avi-natan@users.noreply.github.com>
2019-08-05 23:47:56 +0300

    Working on the game simulation run loop - presenting players with their personal shops over 30 rounds

--- tfttrader/game/game.py
+++ tfttrader/game/game.py
@@ -22,3 +22,25 @@ class Game:
         print(self.pool.quantities)
         for p in self.players:
             print(p.to_string())
+
+    def run(self):
+        i = 1
+        while i <= 30:
+            print("Iteration number:    {}".format(i))
+            for p in self.players:
+                print("Shop for player:     {}".format(p.name))
+                for j in range(0, 5):
+                    # TODO: put a fine-super-crazy-fine algorithm to randomize champions from the pool
+                    # champpos = random.randrange(0, self.pool.total)
+                    # cp = champpos
+                    # ci = 0
+                    # champ_name = ""
+                    # while cp > 0:
+                    #     if self.pool.quantities[ci] < cp:
+                    #         cp -= self.pool.quantities[ci]
+                    #     else:
+                    #         champ_name = self.pool.quantities[ci]
+                    print("         position {}: {}".format(j, self.pool.quantities[0][0]), end="")
+                print("\n")
+            print("\n\n")
+            i += 1
--- tfttrader/game/pool.py
+++ tfttrader/game/pool.py
@@ -13,63 +13,76 @@ quantity_level_3 = 21
 quantity_level_4 = 13
 quantity_level_5 = 10
 
+TOTAL_LVL1_NUM = 468
+TOTAL_LVL2_NUM = 312
+TOTAL_LVL3_NUM = 252
+TOTAL_LVL4_NUM = 117
+TOTAL_LVL5_NUM = 60
+
+TOTAL_CHAMPS_NUM = TOTAL_LVL1_NUM + TOTAL_LVL2_NUM + TOTAL_LVL3_NUM + TOTAL_LVL4_NUM + TOTAL_LVL5_NUM
+
 
 class Pool:
     def __init__(self):
-        self.quantities = {
-            "Darius": (cost_level_1, quantity_level_1),
-            "Elise": (cost_level_1, quantity_level_1),
-            "Fiora": (cost_level_1, quantity_level_1),
-            "Garen": (cost_level_1, quantity_level_1),
-            "Graves": (cost_level_1, quantity_level_1),
-            "Kassadin": (cost_level_1, quantity_level_1),
-            "Kha'Zix": (cost_level_1, quantity_level_1),
-            "Mordekaiser": (cost_level_1, quantity_level_1),
-            "Nidalee": (cost_level_1, quantity_level_1),
-            "Tristana": (cost_level_1, quantity_level_1),
-            "Vayne": (cost_level_1, quantity_level_1),
-            "Warwick": (cost_level_1, quantity_level_1),
+        self.total = TOTAL_CHAMPS_NUM
+        self.quantities = [
+            ["Darius", cost_level_1, quantity_level_1],
+            ["Elise", cost_level_1, quantity_level_1],
+            ["Fiora", cost_level_1, quantity_level_1],
+        ]
 
-            "Ahri": (cost_level_2, quantity_level_2),
-            "Blitzcrank": (cost_level_2, quantity_level_2),
-            "Braum": (cost_level_2, quantity_level_2),
-            "Lissandra": (cost_level_2, quantity_level_2),
-            "Lucian": (cost_level_2, quantity_level_2),
-            "Lulu": (cost_level_2, quantity_level_2),
-            "Pyke": (cost_level_2, quantity_level_2),
-            "Rek'Sai": (cost_level_2, quantity_level_2),
-            "Shen": (cost_level_2, quantity_level_2),
-            "Twisted Fate": (cost_level_2, quantity_level_2),
-            "Varus": (cost_level_2, quantity_level_2),
-            "Zed": (cost_level_2, quantity_level_2),
+        # TODO: get all the other champions below to the array above
 
-            "Aatrox": (cost_level_3, quantity_level_3),
-            "Ashe": (cost_level_3, quantity_level_3),
-            "Evelynn": (cost_level_3, quantity_level_3),
-            "Gangplank": (cost_level_3, quantity_level_3),
-            "Katarina": (cost_level_3, quantity_level_3),
-            "Kennen": (cost_level_3, quantity_level_3),
-            "Morgana": (cost_level_3, quantity_level_3),
-            "Poppy": (cost_level_3, quantity_level_3),
-            "Rengar": (cost_level_3, quantity_level_3),
-            "Shyvana": (cost_level_3, quantity_level_3),
-            "Veigar": (cost_level_3, quantity_level_3),
-            "Volibear": (cost_level_3, quantity_level_3),
-
-            "Akali": (cost_level_4, quantity_level_4),
-            "Aurelion Sol": (cost_level_4, quantity_level_4),
-            "Brand": (cost_level_4, quantity_level_4),
-            "Cho'Gath": (cost_level_4, quantity_level_4),
-            "Draven": (cost_level_4, quantity_level_4),
-            "Gnar": (cost_level_4, quantity_level_4),
-            "Kindred": (cost_level_4, quantity_level_4),
-            "Leona": (cost_level_4, quantity_level_4),
-            "Sejuani": (cost_level_4, quantity_level_4),
-
-            "Anivia": (cost_level_5, quantity_level_5),
-            "Karthus": (cost_level_5, quantity_level_5),
-            "Kayle": (cost_level_5, quantity_level_5),
-            "Miss Fortune": (cost_level_5, quantity_level_5),
-            "Swain": (cost_level_5, quantity_level_5),
-            "Yasuo": (cost_level_5, quantity_level_5),
-        }
+        #     "Garen": (cost_level_1, quantity_level_1),
+        #     "Graves": (cost_level_1, quantity_level_1),
+        #     "Kassadin": (cost_level_1, quantity_level_1),
+        #     "Kha'Zix": (cost_level_1, quantity_level_1),
+        #     "Mordekaiser": (cost_level_1, quantity_level_1),
+        #     "Nidalee": (cost_level_1, quantity_level_1),
+        #     "Tristana": (cost_level_1, quantity_level_1),
+        #     "Vayne": (cost_level_1, quantity_level_1),
+        #     "Warwick": (cost_level_1, quantity_level_1),
+        #
+        #     "Ahri": (cost_level_2, quantity_level_2),
+        #     "Blitzcrank": (cost_level_2, quantity_level_2),
+        #     "Braum": (cost_level_2, quantity_level_2),
+        #     "Lissandra": (cost_level_2, quantity_level_2),
+        #     "Lucian": (cost_level_2, quantity_level_2),
+        #     "Lulu": (cost_level_2, quantity_level_2),
+        #     "Pyke": (cost_level_2, quantity_level_2),
+        #     "Rek'Sai": (cost_level_2, quantity_level_2),
+        #     "Shen": (cost_level_2, quantity_level_2),
+        #     "Twisted Fate": (cost_level_2, quantity_level_2),
+        #     "Varus": (cost_level_2, quantity_level_2),
+        #     "Zed": (cost_level_2, quantity_level_2),
+        #
+        #     "Aatrox": (cost_level_3, quantity_level_3),
+        #     "Ashe": (cost_level_3, quantity_level_3),
+        #     "Evelynn": (cost_level_3, quantity_level_3),
+        #     "Gangplank": (cost_level_3, quantity_level_3),
+        #     "Katarina": (cost_level_3, quantity_level_3),
+        #     "Kennen": (cost_level_3, quantity_level_3),
+        #     "Morgana": (cost_level_3, quantity_level_3),
+        #     "Poppy": (cost_level_3, quantity_level_3),
+        #     "Rengar": (cost_level_3, quantity_level_3),
+        #     "Shyvana": (cost_level_3, quantity_level_3),
+        #     "Veigar": (cost_level_3, quantity_level_3),
+        #     "Volibear": (cost_level_3, quantity_level_3),
+        #
+        #     "Akali": (cost_level_4, quantity_level_4),
+        #     "Aurelion Sol": (cost_level_4, quantity_level_4),
+        #     "Brand": (cost_level_4, quantity_level_4),
+        #     "Cho'Gath": (cost_level_4, quantity_level_4),
+        #     "Draven": (cost_level_4, quantity_level_4),
+        #     "Gnar": (cost_level_4, quantity_level_4),
+        #     "Kindred": (cost_level_4, quantity_level_4),
+        #     "Leona": (cost_level_4, quantity_level_4),
+        #     "Sejuani": (cost_level_4, quantity_level_4),
+        #
+        #     "Anivia": (cost_level_5, quantity_level_5),
+        #     "Karthus": (cost_level_5, quantity_level_5),
+        #     "Kayle": (cost_level_5, quantity_level_5),
+        #     "Miss Fortune": (cost_level_5, quantity_level_5),
+        #     "Swain": (cost_level_5, quantity_level_5),
+        #     "Yasuo": (cost_level_5, quantity_level_5),
+        # }
